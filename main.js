6a8bf6e2eeb185b5bb0c43a935f2b1c236df5b3b
Agus188919 <floppy_10_mm@hotmail.com>
2025-10-02 21:35:06 -0300

    Generación de cards y dark-mode

--- main.js
+++ main.js
@@ -1,22 +1,22 @@
-const btn  = document.querySelector('.hamburger');
-  const menu = document.getElementById('main-menu');
+const dark = document.getElementById("darkModeToggle");
 
-  function closeMenu() {
-    menu.classList.remove('open');
-    btn.classList.remove('is-open');
-    btn.setAttribute('aria-expanded', 'false');
-    document.body.classList.remove('noscroll');
+function closeMenu() {
+  menu.classList.remove('open');
+  btn.classList.remove('is-open');
+  btn.setAttribute('aria-expanded', 'false');
+  document.body.classList.remove('noscroll');
+}
+
+
+document.addEventListener('keydown', e => { if (e.key === 'Escape') closeMenu(); });
+document.addEventListener('click', e => {
+  if (!menu.contains(e.target) && !btn.contains(e.target)) closeMenu();
+});
+dark.addEventListener("click", function () {
+  document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    this.textContent = "☀️";
+  } else {
+    this.textContent = "🌙";
   }
-
-  btn.addEventListener('click', () => {
-    const isOpen = menu.classList.toggle('open');
-    btn.classList.toggle('is-open', isOpen);
-    btn.setAttribute('aria-expanded', String(isOpen));
-    document.body.classList.toggle('noscroll', isOpen);
-  });
-
-  menu.addEventListener('click', e => { if (e.target.tagName === 'A') closeMenu(); });
-  document.addEventListener('keydown', e => { if (e.key === 'Escape') closeMenu(); });
-  document.addEventListener('click', e => {
-    if (!menu.contains(e.target) && !btn.contains(e.target)) closeMenu();
-  });
+});
